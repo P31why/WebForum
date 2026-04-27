94113cf429c2a4f0b2f57df14f0dfa1ba42cc30b
Hidan Entity <killer.xtale.suns@mail.ru>
2026-04-27 23:03:21 +0300

    сделал BaseController абстрактным

--- WebForum/Controllers/BaseController.cs
+++ WebForum/Controllers/BaseController.cs
@@ -4,7 +4,7 @@ namespace WebForum.WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BaseController : ControllerBase
+    public abstract class BaseController : ControllerBase
     {
     }
 }
