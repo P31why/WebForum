97cb6dc374fc4c5cd78a2efc6157090837296a08
Stas <bil.sheper5a@gmail.com>
2026-04-27 12:25:21 +0300

    добавил scalar для визуализации, тестирования и документирования api

--- WebForum/Program.cs
+++ WebForum/Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace WebForum
 {
     public class Program
@@ -19,6 +21,7 @@ namespace WebForum
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
