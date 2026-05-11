9b146e419e36278c251a65efca1ad2fbb4264e35
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-11 22:34:03 +0300

    интерфейс для взаимодействия с таблицами у которых могут быть id разного типа

---
+++ WebForum.Data/IId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebForum.Infrastructure
+{
+    public interface IId<Tkey>
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        Tkey Id { get; set; }
+    }
+}
