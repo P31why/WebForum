a35eb725f42a0eeca4a9a458113f43adf5f2562c
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-11 22:34:26 +0300

    добавлен контекст бд

--- WebForum.Data/AppDbContext.cs
+++ WebForum.Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebForum.Data.Entities;
 
 namespace WebForum.Data
 {
     public class AppDbContext : DbContext
     {
-
+        public DbSet<User> Users { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options){}
     }
 }
