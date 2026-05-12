ac3c6d67d7ad00dd67ea070d10fe55ef6369d17b
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-12 21:50:21 +0300

    добавлен для того чтобы удобнее писать миграции бд

---
+++ WebForum.Data/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using WebForum.Data;
+
+namespace WebForum.Infrastructure
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "", "WebForum.Api"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+            return new AppDbContext(builder.Options);
+        }
+    }
+}
