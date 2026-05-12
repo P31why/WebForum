e0046d072efcbd4dbad32bed6874348969d40164
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-12 22:12:30 +0300

    добавил интерфейс репозитория (покачто не доработан)

---
+++ WebForum.Data/Interfaces/IRepositry.cs
@@ -0,0 +1,8 @@
+﻿
+namespace WebForum.Infrastructure.Interfaces
+{
+    public interface IRepositry<TEntity,TKey> where TEntity : class
+    {
+        Task<TEntity> GetEntityAsync(TKey id, IQueryable<TEntity> include);
+    }
+}
