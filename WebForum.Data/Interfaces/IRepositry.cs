578b9aa1b9a2f48530bfca70bd46ce7c02bca8c7
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-16 22:47:45 +0300

    начал делать интерфейсы для отдельных репозитоориев

--- WebForum.Data/Interfaces/IRepositry.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace WebForum.Infrastructure.Interfaces
-{
-    public interface IRepositry<TEntity,TKey> where TEntity : class
-    {
-        Task<TEntity> GetEntityAsync(TKey id, IQueryable<TEntity> include);
-    }
-}
---
+++ WebForum.Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using WebForum.Core;
+
+namespace WebForum.Infrastructure.Interfaces
+{
+    public interface IUserRepository<TKey>
+    {
+        Task CreatUserEntityAsync();
+        
+        Task GetAllUsersAsync(TKey userId, UserGetInfo type);
+        
+        Task UpdateUserEntityAsync();
+
+        Task DeleteUserEntityAsync(TKey userId);
+    }
+}
