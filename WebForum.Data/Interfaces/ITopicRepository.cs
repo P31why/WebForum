15395253781d03b7ca7d0578cde468cf94f90e11
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-24 22:16:36 +0300

    начал добавлять репозиторий "треда"

---
+++ WebForum.Data/Interfaces/ITopicRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using WebForum.Core.Models;
+
+namespace WebForum.Infrastructure.Interfaces
+{
+    public interface ITopicRepository
+    {
+        public Task UpdateEntityAsync(TopicDto topicDto);
+
+        public Task<TopicDto> GetDtoAsync(Guid topicId);
+
+        public Task<TopicShortDto> GetShortDtoAsync(Guid topicId);
+
+        public Task<IReadOnlyCollection<TopicDto>> GetCollectionDtoAsync(Guid? userId);
+
+        public Task<IReadOnlyCollection<TopicShortDto>> GetCollectionDtoShortAsync(Guid? userId);
+    }
+}
---
+++ WebForum.Data/Repository/TopicRepository.cs
@@ -0,0 +1,75 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebForum.Core.Models;
+using WebForum.Data;
+using WebForum.Infrastructure.Entities;
+using WebForum.Infrastructure.Interfaces;
+
+namespace WebForum.Infrastructure.Repository
+{
+    public class TopicRepository(AppDbContext dbContext) : BaseRepository<Guid, Topic>(dbContext), ITopicRepository
+    {
+        public async Task<IReadOnlyCollection<TopicDto>> GetCollectionDtoAsync(Guid? userId)
+        {
+            if(userId == null)
+            {
+                return await dbContext.Topics.Select(i => new TopicDto
+                                             {
+                                                 Id = i.Id,
+                                                 UserId = i.UserId,
+                                                 Title = i.Title,
+                                                 Description = i.Description
+                                             }).AsNoTracking().ToListAsync();
+            }
+            else
+            {
+                return await dbContext.Topics.Where(t => t.UserId == userId)
+                                             .Select(i => new TopicDto
+                                             {
+                                                 Id = i.Id,
+                                                 UserId = i.UserId,
+                                                 Title = i.Title,
+                                                 Description = i.Description
+                                             }).AsNoTracking() .ToListAsync();
+
+            }
+        }
+
+        public async Task<IReadOnlyCollection<TopicShortDto>> GetCollectionDtoShortAsync(Guid? userId)
+        {
+            if (userId == null)
+            {
+                return await dbContext.Topics.Select(i => new TopicShortDto
+                                             {
+                                                 Id = i.Id,
+                                                 Title = i.Title,
+                                             }).AsNoTracking().ToListAsync();
+            }
+            else
+            {
+                return await dbContext.Topics.Where(t => t.UserId == userId)
+                                             .Select(i => new TopicShortDto
+                                             {
+                                                 Id = i.Id,
+                                                 Title = i.Title,
+                                             }).AsNoTracking().ToListAsync();
+
+            }
+        }
+
+        public Task<TopicDto> GetDtoAsync(Guid topicId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TopicShortDto> GetShortDtoAsync(Guid topicId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEntityAsync(TopicDto topicDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
