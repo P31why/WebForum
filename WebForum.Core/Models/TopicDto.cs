437324a6a8313cb5a29ec262c9a2e38a2df05b65
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-24 22:13:03 +0300

    добавил новые модели для тредов

---
+++ WebForum.Core/Models/TopicDto.cs
@@ -0,0 +1,18 @@
+﻿
+namespace WebForum.Core.Models
+{
+    public class TopicDto : TopicShortDto
+    {
+        public Guid UserId { get; init; }
+        
+        public string? Description { get; set; }
+    }
+
+    public class TopicShortDto
+    {
+        public Guid Id { get; init; }
+
+        public string Title { get; set; }
+
+    }
+}
