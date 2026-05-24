07b5c02ed5b10a368154a015f92acba1dca589d2
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-24 22:13:49 +0300

    решил сделать базовый репозиторий чтобы потом в него сгонять повторяющиеся методы

---
+++ WebForum.Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using WebForum.Core;
+
+namespace WebForum.Infrastructure.Interfaces
+{
+    public interface IBaseRepository<Tkey, TEntity> where TEntity : class
+    {
+        public Task CreateEntityAsync(TEntity entity);
+
+        public Task CommitDbAsync();
+
+        public Task DeleteEntityAsync(Tkey tkey, DeleteType type);
+    }
+}
---
+++ WebForum.Data/Repository/BaseRepository.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebForum.Core;
+using WebForum.Data;
+using WebForum.Infrastructure.Interfaces;
+
+namespace WebForum.Infrastructure.Repository
+{
+    public class BaseRepository<TKey, TEntity> (AppDbContext dbContext): IBaseRepository<TKey, TEntity> where TEntity : class
+    {
+
+        private readonly DbSet<TEntity> _dbSet = dbContext.Set<TEntity>();
+
+        public async Task CreateEntityAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task CommitDbAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteEntityAsync(TKey tkey, DeleteType type)
+        {
+
+            if (type == DeleteType.NoVisible)
+                await _dbSet.Where(e => EF.Property<TKey>(e, "Id").Equals(tkey))
+                                                              .ExecuteDeleteAsync();
+
+        }
+    }
+}
