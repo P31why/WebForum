2db58ccd115d980c7b1cb1d1086a6272e2ebf18a
Hidan Entity <killer.xtale.suns@mail.ru>
2026-05-24 22:15:38 +0300

    добавил новые поля DbSet

--- WebForum.Data/AppDbContext.cs
+++ WebForum.Data/AppDbContext.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebForum.Data.Entities;
+using WebForum.Infrastructure.Entities;
 
 namespace WebForum.Data
 {
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Topic> Topics { get; set; }
+
+        public DbSet<Post> Post { get; set; }
+
+        public DbSet<Comment> Comment { get; set; }
+
         public AppDbContext(DbContextOptions options) : base(options){}
     }
 }
