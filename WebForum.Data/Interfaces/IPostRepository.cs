16be8281858bcfa7eef00ccc9a36ffc42f1fb4b5
P31Dead <P31Dead@DESKTOP-NALDILN>
2026-05-26 22:55:47 +0300

    дбавил все методы для репозитория

--- WebForum.Data/Interfaces/IPostRepository.cs
+++ WebForum.Data/Interfaces/IPostRepository.cs
@@ -1,8 +1,18 @@
-﻿
+﻿using WebForum.Core.Models;
+
 namespace WebForum.Infrastructure.Interfaces
 {
     public interface IPostRepository
     {
+        public Task UpdateEntityAsync(PostDto postDto);
 
+        public Task<PostDto> GetDtoAsync(Guid postId);
+
+        public Task<PostShortDto> GetShortDtoAsync(Guid postId);
+
+        public Task<IReadOnlyList<PostDto>> GetCollectionDtoAsync(Guid? userId);
+
+        public Task<IReadOnlyList<PostShortDto>> GetCollectionShortDtoAsync(Guid? userId);
+        
     }
 }
