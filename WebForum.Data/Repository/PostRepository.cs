1090c7708e2b4d0bd9866bf76948f97dd7450130
P31Dead <P31Dead@DESKTOP-NALDILN>
2026-05-26 22:58:07 +0300

    продолжаю реализовывать репозиторий, сгенерированы методы интерфейса и добавлено наслеование от базового репозитория

--- WebForum.Data/Repository/PostRepository.cs
+++ WebForum.Data/Repository/PostRepository.cs
@@ -1,7 +1,44 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+using WebForum.Core.Models;
+using WebForum.Data;
+using WebForum.Infrastructure.Entities;
+using WebForum.Infrastructure.Interfaces;
+
 namespace WebForum.Infrastructure.Repository
 {
-    public class PostRepository
+    public class PostRepository(AppDbContext dbContext) : BaseRepository<Guid, Post>(dbContext), IPostRepository
     {
+        public Task<IReadOnlyList<PostDto>> GetCollectionDtoAsync(Guid? userId)
+        {
+            return dbContext.Post
+                .Where(p => p.UserId == userId)
+                .Select(i => new PostDto
+                {
+                    Id = i.Id,
+                    TopicId = i.TopicId,
+
+                })
+        }
+
+        public Task<IReadOnlyList<PostShortDto>> GetCollectionShortDtoAsync(Guid? userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDto> GetDtoAsync(Guid postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostShortDto> GetShortDtoAsync(Guid postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEntityAsync(PostDto postDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
