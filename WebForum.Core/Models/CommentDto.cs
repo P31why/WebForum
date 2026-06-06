c2712daa86ef5ae2d77a9070ba757444f4d41602
P31Dead <P31Dead@DESKTOP-NALDILN>
2026-06-06 23:55:23 +0300

    добавлена новая модель

---
+++ WebForum.Core/Models/CommentDto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WebForum.Core.Models
+{
+    public class CommentDto 
+    {
+        public long Id { get; set; }
+
+        public Guid PostId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public required string Text { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
