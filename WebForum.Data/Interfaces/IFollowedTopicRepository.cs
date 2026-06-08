1f8a2e1a26bf0e90661d2ca4970e606f7abb07db
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 01:29:59 +0300

    добавил репозиторий для таблицы отслеживаний топиков

---
+++ WebForum.Data/Interfaces/IFollowedTopicRepository.cs
@@ -0,0 +1,10 @@
+﻿
+using WebForum.Core.Models;
+
+namespace WebForum.Infrastructure.Interfaces
+{
+    public interface IFollowedTopicRepository
+    {
+        public Task<IReadOnlyCollection<FollowedTopicDto>?> GetAllAsync(Guid userId);
+    }
+}
---
+++ WebForum.Data/Repository/FollowedTopicRepository.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebForum.Core.Models;
+using WebForum.Data;
+using WebForum.Infrastructure.Entities;
+using WebForum.Infrastructure.Interfaces;
+
+namespace WebForum.Infrastructure.Repository
+{
+    public class FollowedTopicRepository(AppDbContext dbContext) : BaseRepository<long, FollowedTopic>(dbContext), IFollowedTopicRepository
+    {
+        public async Task<IReadOnlyCollection<FollowedTopicDto>?> GetAllAsync(Guid userId)
+        {
+            return (await _dbSet
+                .Where(ft => ft.UserId == userId)
+                .Select(i => new FollowedTopicDto
+                {
+                    Id = i.Id,
+                    UserId = i.UserId,
+                    TopicId = i.TopicId,
+                })
+                .AsNoTracking()
+                .ToListAsync());
+        }
+    }
+}
