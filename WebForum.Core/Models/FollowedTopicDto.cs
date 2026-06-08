200f899e11a9cbad11e42a76764a5d95101036dc
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 01:24:19 +0300

    добавлена модель отследиваемых топиков

---
+++ WebForum.Core/Models/FollowedTopicDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WebForum.Core.Models
+{
+    public class FollowedTopicDto
+    {
+        public long Id { get; set; }
+
+        public Guid UserId {  get; set; }
+
+        public Guid TopicId { get; set; }
+    }
+}
