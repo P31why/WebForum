72bcc50f7d6051200cb8794bece03964cbfd7327
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 02:22:00 +0300

    добавлены миграции

---
+++ WebForum.Data/Migrations/20260608015325_added-table-followed_post.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebForum.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedtablefollowed_post : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FollowedTopic",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TopicId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FollowedTopic", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FollowedTopic_Topics_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topics",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_FollowedTopic_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FollowedTopic_TopicId",
+                table: "FollowedTopic",
+                column: "TopicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FollowedTopic_UserId_TopicId",
+                table: "FollowedTopic",
+                columns: new[] { "UserId", "TopicId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FollowedTopic");
+        }
+    }
+}
