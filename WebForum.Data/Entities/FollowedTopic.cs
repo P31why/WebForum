297319788c3b6c711d79e04aa1a600c852ce810e
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 01:25:44 +0300

    добавлена таблица отслеживаемых топиков

---
+++ WebForum.Data/Entities/FollowedTopic.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using WebForum.Data.Entities;
+
+namespace WebForum.Infrastructure.Entities
+{
+    public class FollowedTopic : IId<long>
+    {
+        public long Id { get; set; }
+
+        public Guid TopicId { get; set; }
+
+        [ForeignKey(nameof(TopicId))]
+        public required Topic Topic { get; set; }
+
+        public Guid UserId { get; set; }
+        
+        [ForeignKey(nameof(UserId))]
+        public required User User { get; set; }
+    }
+}
