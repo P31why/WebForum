c55ee5b793e4cadf4936539e01cde2541f1e6208
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 01:20:26 +0300

    создан сервис для работы с топиками

---
+++ WebForum.Application/User/Interfaces/ITopicService.cs
@@ -0,0 +1,14 @@
+﻿using WebForum.Core.Models;
+
+namespace WebForum.Application.User.Interface
+{
+
+    public interface ITopicService
+    {
+        public Task<IReadOnlyCollection<FollowedTopicDto>> GetAllFollowedTopicsAsync(Guid userId);
+
+        public Task<IReadOnlyCollection<TopicShortDto>> GetAllShortAsync(Guid? userId = null);
+
+        public Task<TopicDto> GetByIdAsync(Guid id);
+    }
+}
---
+++ WebForum.Application/User/Services/TopicService.cs
@@ -0,0 +1,24 @@
+﻿using WebForum.Application.User.Interface;
+using WebForum.Core.Models;
+using WebForum.Infrastructure.Interfaces;
+
+namespace WebForum.Application.User.Services
+{
+    public class TopicService(ITopicRepository topicRepository, IFollowedTopicRepository followedTopicRepository) : ITopicService
+    {
+        public async Task<IReadOnlyCollection<FollowedTopicDto>> GetAllFollowedTopicsAsync(Guid userId)
+        {
+            return (await followedTopicRepository.GetAllAsync(userId)) ?? Array.Empty<FollowedTopicDto>();
+        }
+
+        public async Task<IReadOnlyCollection<TopicShortDto>> GetAllShortAsync(Guid? userId = null)
+        {
+            return (await topicRepository.GetCollectionShortDtoAsync(userId)) ?? Array.Empty<TopicShortDto>();
+        }
+
+        public async Task<TopicDto> GetByIdAsync(Guid id)
+        {
+            return await topicRepository.GetDtoAsync(id);
+        }
+    }
+}
