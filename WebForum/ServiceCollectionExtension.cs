19b6b775510974ee2edc78b5fe771e58d7faf64f
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 01:18:25 +0300

    зарегестрировал сервисы и репозитории в отдельном статическом методе класса

--- WebForum/ServiceCollectionExtension.cs
+++ WebForum/ServiceCollectionExtension.cs
@@ -1,6 +1,39 @@
-﻿namespace WebForum.WebApi
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using WebForum.Application.User.Interface;
+using WebForum.Application.User.Interfaces;
+using WebForum.Application.User.Services;
+using WebForum.Data;
+using WebForum.Data.Entities;
+using WebForum.Infrastructure.Entities;
+using WebForum.Infrastructure.Interfaces;
+using WebForum.Infrastructure.Repository;
+
+namespace WebForum.WebApi
 {
-    public class ServiceCollectionExtension
+    public static class ServiceCollectionExtension
     {
+        public static void ConfigureOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+
+            services.AddScoped<IBaseRepository<Guid, Topic>, BaseRepository<Guid, Topic>>();
+            services.AddScoped<IBaseRepository<Guid, User>, BaseRepository<Guid, User>>();
+            services.AddScoped<IBaseRepository<Guid, Post>, BaseRepository<Guid, Post>>();
+            services.AddScoped<IBaseRepository<long, Comment>, BaseRepository<long, Comment>>();
+            services.AddScoped<IBaseRepository<long, FollowedTopic>, BaseRepository<long, FollowedTopic>>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ITopicRepository, TopicRepository>();
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IFollowedTopicRepository, FollowedTopicRepository>();
+
+            services.AddScoped<ITopicService, TopicService>();
+            services.AddScoped<IPostService, PostService>();
+
+        }
     }
 }
