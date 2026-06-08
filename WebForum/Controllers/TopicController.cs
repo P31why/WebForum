6b2201ffe1e4ff1cf74c2d7ef9b9a912f2bde89d
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 01:16:52 +0300

    добавлен контроллер топиков(тем)

---
+++ WebForum/Controllers/TopicController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebForum.Application.User.Interface;
+using WebForum.Core.Models;
+
+namespace WebForum.WebApi.Controllers
+{
+    public class TopicController(ITopicService service) : BaseController
+    {
+        [HttpGet("GetAllFollowedAsync")]
+        public async Task<IReadOnlyCollection<FollowedTopicDto>> GetAllFollowedAsync(Guid userId)
+        {
+            return await service.GetAllFollowedTopicsAsync(userId);
+        }
+
+        [HttpGet("GetAllAsync")]
+        public async Task<IReadOnlyCollection<TopicShortDto>> GetAllAsync(Guid? userId = null)
+        {
+            return await service.GetAllShortAsync(userId);
+        }
+
+        [HttpGet("GetTopicAsync")]
+        public async Task<TopicDto> GetTopicAsync(Guid topicId)
+        {
+            return await service.GetByIdAsync(topicId);
+        }
+    }
+}
