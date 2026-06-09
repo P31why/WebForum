17516523f21f66e0b3622b6b082f8795edfaed3b
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 03:12:18 +0300

    добавлена еще пачка миграций бд

--- WebForum.Data/Migrations/20260609000743_added-is_deleted-feilds.Designer.cs
+++ WebForum.Data/Migrations/20260609000743_added-is_deleted-feilds.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebForum.Data;
 
@@ -11,9 +12,11 @@ using WebForum.Data;
 namespace WebForum.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260609000743_added-is_deleted-feilds")]
+    partial class addedis_deletedfeilds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
