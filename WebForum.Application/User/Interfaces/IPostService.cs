034c2f9f4b65a0c9d9770de69321cc7891375f86
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-09 03:10:41 +0300

    добавлен сервис постов

---
+++ WebForum.Application/User/Interfaces/IPostService.cs
@@ -0,0 +1,19 @@
+﻿
+using WebForum.Core;
+using WebForum.Core.Models;
+
+namespace WebForum.Application.User.Interfaces
+{
+    public interface IPostService
+    {
+        public Task<PostShortDto> GetAllShortAsync(Guid topicId);
+
+        public Task<PostDto> GetByIdAsync(Guid postId);
+
+        public Task<PostDto> AddPostAsync(PostDto postDto);
+
+        public Task<bool> UpdatePostAsync (PostDto postDto);
+
+        public Task<bool> DeletePostAsync (Guid topicId, DeleteType type = DeleteType.NoVisible);
+    }
+}
---
+++ WebForum.Application/User/Services/PostService.cs
@@ -0,0 +1,37 @@
+﻿
+using WebForum.Application.User.Interfaces;
+using WebForum.Core;
+using WebForum.Core.Models;
+using WebForum.Infrastructure.Interfaces;
+
+namespace WebForum.Application.User.Services
+{
+    public class PostService(IPostRepository repository) : IPostService
+    {
+        public Task<PostDto> AddPostAsync(PostDto postDto)
+        {
+            //repository.CreateEntityAsync();
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePostAsync(Guid topicId, DeleteType type = DeleteType.NoVisible)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostShortDto> GetAllShortAsync(Guid topicId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PostDto> GetByIdAsync(Guid postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePostAsync(PostDto postDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
