1212500ff858192ae9eebd0e5d59fb5ab3d8503e
P31Dead <P31Dead@DESKTOP-44E946F>
2026-06-14 18:56:26 +0300

    написан сервис для сохранения отслеживаемых топиков

---
+++ WebForum.Application/User/Interfaces/IFollowTopicService.cs
@@ -0,0 +1,15 @@
+﻿
+using WebForum.Core;
+using WebForum.Core.Models;
+
+namespace WebForum.Application.User.Interfaces
+{
+    public interface IFollowTopicService
+    {
+        public Task<IReadOnlyCollection<FollowedTopicDto>> GetAllFollowedTopicsAsync(Guid userId);
+
+        public Task<FollowedTopicDto> AddAsync(FollowedTopicDto dto);
+
+        public Task<bool> DeleteAsync(long Id, DeleteType type);
+    }
+}
---
+++ WebForum.Application/User/Services/FollowedTopicsService.cs
@@ -0,0 +1,35 @@
+﻿
+using WebForum.Application.User.Interfaces;
+using WebForum.Core;
+using WebForum.Core.Models;
+using WebForum.Infrastructure.Interfaces;
+using WebForum.Infrastructure.Mappers;
+
+namespace WebForum.Application.User.Services
+{
+    public class FollowedTopicsService(IFollowedTopicRepository repository,
+                                       FollowedTopicsMapper mapper) : IFollowTopicService
+    {
+        public async Task<FollowedTopicDto> AddAsync(FollowedTopicDto dto)
+        {
+            var entity = await repository.CreateEntityAsync(mapper.DtoToEntity(dto));
+
+            bool isCreated = await repository.CommitDbAsync();
+
+            if (!isCreated)
+                throw new Exception("Error with creating FollowedTopic entity");
+
+            return mapper.EntityToDto(entity);
+        }
+
+        public async Task<bool> DeleteAsync(long id, DeleteType type = DeleteType.Full)
+        {
+            return await repository.DeleteEntityAsync(id, type);
+        }
+
+        public async Task<IReadOnlyCollection<FollowedTopicDto>> GetAllFollowedTopicsAsync(Guid userId)
+        {
+            return await repository.GetAllAsync(userId) ?? Array.Empty<FollowedTopicDto>();
+        }
+    }
+}
