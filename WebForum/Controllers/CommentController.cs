adbef07033ababd951a09af0699e1be90fbd38be
P31Dead <P31Dead@DESKTOP-44E946F>
2026-06-14 18:48:40 +0300

    добавлен контроллер комментариев

---
+++ WebForum/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebForum.Application.User.Interfaces;
+using WebForum.Core;
+using WebForum.Core.Models;
+
+namespace WebForum.WebApi.Controllers
+{
+    public class CommentController(ICommentService service) : BaseController
+    {
+        [HttpGet(nameof(GetAllAsync))]
+        public async Task<IReadOnlyCollection<CommentDto>> GetAllAsync(Guid id, IdType type)
+        {
+            return await service.GetAllAsync(id, type);
+        }
+
+        [HttpPost(nameof(AddAsync))]
+        public async Task<CommentDto> AddAsync(CommentDto commentDto)
+        {
+            return await service.AddAsync(commentDto);
+        }
+
+        [HttpPost(nameof(UpdateAync))]
+        public async Task<CommentDto> UpdateAync(CommentDto commentDto)
+        {
+            return await service.UpdateAsync(commentDto);
+        }
+
+        [HttpDelete(nameof(DeleteAsync))]
+        public async Task<bool> DeleteAsync(long id)
+        {
+            //TODO: second variant deleting
+            return await service.DeleteAsync(id, DeleteType.Full);
+        }
+    }
+}
