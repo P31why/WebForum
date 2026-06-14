a49ec252098ebff243ee5db03eee5b027b76eb88
P31Dead <P31Dead@DESKTOP-44E946F>
2026-06-14 18:50:13 +0300

    добавлен сервис комментов

---
+++ WebForum.Application/User/Interfaces/ICommentService.cs
@@ -0,0 +1,17 @@
+﻿
+using WebForum.Core;
+using WebForum.Core.Models;
+
+namespace WebForum.Application.User.Interfaces
+{
+    public interface ICommentService
+    {
+        public Task<IReadOnlyCollection<CommentDto>> GetAllAsync(Guid postId, IdType type);
+
+        public Task<CommentDto> AddAsync(CommentDto dto);
+
+        public Task<CommentDto> UpdateAsync(CommentDto dto);
+
+        public Task<bool> DeleteAsync(long id, DeleteType type);
+    }
+}
---
+++ WebForum.Application/User/Services/CommentService.cs
@@ -0,0 +1,40 @@
+﻿
+using WebForum.Application.User.Interfaces;
+using WebForum.Core;
+using WebForum.Core.Models;
+using WebForum.Infrastructure.Interfaces;
+using WebForum.Infrastructure.Mappers;
+
+namespace WebForum.Application.User.Services
+{
+    public class CommentService(ICommentRepository repository, CommentMapper mapper) : ICommentService
+    {
+        public async Task<CommentDto> AddAsync(CommentDto dto)
+        {
+            var entity = await repository.CreateEntityAsync(mapper.DtoToEntity(dto));
+
+            bool isCreated = await repository.CommitDbAsync();
+
+            if (!isCreated)
+                throw new Exception("Error with creating comment");
+
+            return mapper.EntityToDto(entity);
+        }
+
+        public async Task<bool> DeleteAsync(long id, DeleteType type = DeleteType.Full)
+        {
+            //TODO: second deleting variant
+            return await repository.DeleteEntityAsync(id, type);
+        }
+
+        public async Task<IReadOnlyCollection<CommentDto>> GetAllAsync(Guid postId, IdType type = IdType.Entity)
+        {
+            return await repository.GetCollectionDtoAsync(postId, type) ?? Array.Empty<CommentDto>();
+        }
+
+        public async Task<CommentDto> UpdateAsync(CommentDto dto)
+        {
+            return await repository.UpdateEntityAsync(dto);
+        }
+    }
+}
