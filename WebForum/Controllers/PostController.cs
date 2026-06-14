70328117b6dd9414b62070122a32352f4b3960ef
P31Dead <P31Dead@DESKTOP-44E946F>
2026-06-14 18:49:21 +0300

    добавлен контроллер постов

---
+++ WebForum/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebForum.Application.User.Interfaces;
+using WebForum.Core;
+using WebForum.Core.Models;
+
+namespace WebForum.WebApi.Controllers
+{
+    public class PostController(IPostService service) : BaseController
+    {
+        [HttpGet(nameof(GetAllAsync))]
+        public async Task<IReadOnlyCollection<PostShortDto>> GetAllAsync(Guid id, IdType type)
+        {
+            return await service.GetAllShortAsync(id, type);
+        }
+
+        [HttpGet(nameof(GetByIdAync))]
+        public async Task<PostDto> GetByIdAync(Guid id)
+        {
+            return await service.GetByIdAsync(id);
+        }
+
+        [HttpPost(nameof(AddAsync))]
+        public async Task<PostDto> AddAsync(PostDto dto)
+        {
+            return await service.AddPostAsync(dto);
+        }
+
+        [HttpPost(nameof(UpdateAsync))]
+        public async Task<PostDto> UpdateAsync(PostDto dto)
+        {
+            return await service.UpdatePostAsync(dto);
+        }
+
+        [HttpDelete(nameof(DeleteAsync))]
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            return await service.DeletePostAsync(id);
+        }
+    }
+}
