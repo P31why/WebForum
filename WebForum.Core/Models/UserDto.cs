df8653587bf5e2209a87d25c5b2a6c6a135c3969
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-22 23:25:40 +0300

    добавлено поле для второго варианта удаления

--- WebForum.Core/Models/UserDto.cs
+++ WebForum.Core/Models/UserDto.cs
@@ -4,11 +4,15 @@ namespace WebForum.Core.Models
     public class UserDto : UserShortDto
     {
         public string? Email { get; set; }
+
+        public DateTime? CreatedDate { get; init; }
     }
 
     public class UserShortDto
     {
         public required Guid Id { get; set; }
+
+        public bool IsDeleted { get; set; }
 
         public required string UserName { get; set; }
     }
