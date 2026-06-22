3b5f0e83e3340ea26470cf201d6abfcd69f5346a
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-22 23:06:46 +0300

    переместил метод в контроллер FollowedTopicController и добавлен запрос на обновление

--- WebForum/Controllers/TopicController.cs
+++ WebForum/Controllers/TopicController.cs
@@ -6,28 +6,28 @@ namespace WebForum.WebApi.Controllers
 {
     public class TopicController(ITopicService service) : BaseController
     {
-        [HttpPost("AddAsync")]
+        [HttpPost(nameof(AddAsync))]
         public async Task<TopicDto> AddAsync(TopicDto topic)
         {
             return await service.AddAsync(topic);
-        }
+        }  
 
-        [HttpGet("GetAllFollowedAsync")]
-        public async Task<IReadOnlyCollection<FollowedTopicDto>> GetAllFollowedAsync(Guid userId)
-        {
-            return await service.GetAllFollowedTopicsAsync(userId);
-        }
-
-        [HttpGet("GetAllAsync")]
+        [HttpGet(nameof(GetAllAsync))]
         public async Task<IReadOnlyCollection<TopicShortDto>> GetAllAsync(Guid? userId = null)
         {
             return await service.GetAllShortAsync(userId);
         }
 
-        [HttpGet("GetTopicAsync")]
+        [HttpGet(nameof(GetTopicAsync))]
         public async Task<TopicDto> GetTopicAsync(Guid topicId)
         {
             return await service.GetByIdAsync(topicId);
+        }
+
+        [HttpPost(nameof(UpdateAsync))]
+        public async Task<bool> UpdateAsync(TopicDto dto)
+        {
+            return await service.UpdateAsync(dto);
         }
     }
 }
