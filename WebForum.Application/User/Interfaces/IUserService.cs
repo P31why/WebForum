d57744b005b03e845f175349fc830bbb603dc1ce
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-22 23:22:32 +0300

    написан сервис для взаимодействия с пользователями

--- WebForum.Application/User/Interfaces/IUserService.cs
+++ WebForum.Application/User/Interfaces/IUserService.cs
@@ -1,8 +1,19 @@
 ﻿
+using WebForum.Core;
+using WebForum.Core.Models;
+
 namespace WebForum.Application.User.Interfaces
 {
     public interface IUserService
     {
+        public Task<UserDto> GetByIdAsync(Guid userId);
 
+        public Task<UserDto> AddAsync(UserDto dto);
+
+        public Task<bool> UpdateAsync(UserDto dto);
+
+        public Task<bool> UpdatePasswordAsync(Guid id, string hash);
+
+        public Task<bool> DeleteAsync(Guid userId, DeleteType type);
     }
 }
--- WebForum.Application/User/Services/UserService.cs
+++ WebForum.Application/User/Services/UserService.cs
@@ -1,10 +1,42 @@
 ﻿
 using WebForum.Application.User.Interfaces;
+using WebForum.Core;
+using WebForum.Core.Models;
+using WebForum.Infrastructure.Interfaces;
+using WebForum.Infrastructure.Mappers;
 
 namespace WebForum.Application.User.Services
 {
-    public class UserService : IUserService
+    public class UserService(IUserRepository repository, UserMapper mapper) : IUserService
     {
+        public async Task<UserDto> AddAsync(UserDto dto)
+        {
+            var entity = await repository.CreateEntityAsync(mapper.DtoToEntity(dto));
 
+            if (entity == null)
+                throw new Exception("User creating error");
+
+            return mapper.EntityToDto(entity);
+        }
+
+        public async Task<bool> DeleteAsync(Guid userId, DeleteType type)
+        {
+            return await repository.DeleteEntityAsync(userId, type);
+        }
+
+        public async Task<UserDto> GetByIdAsync(Guid userId)
+        {
+            return await repository.GetDtoAsync(userId);
+        }
+
+        public async Task<bool> UpdateAsync(UserDto dto)
+        {
+            return await repository.UpdateUserEntityAsync(dto, UserModelType.Full);
+        }
+
+        public async Task<bool> UpdatePasswordAsync(Guid id, string hash)
+        {
+            return await repository.UpdateUserPasswordAsync(id, hash);
+        }
     }
 }
