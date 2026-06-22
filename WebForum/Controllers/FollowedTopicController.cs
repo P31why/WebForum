d40f9d75768d35c33aac28ff60c5a09d890b1dde
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-22 22:43:59 +0300

    добавлен контроллер сохраненных топиков

---
+++ WebForum/Controllers/FollowedTopicController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebForum.Application.User.Interfaces;
+using WebForum.Core;
+using WebForum.Core.Models;
+
+namespace WebForum.WebApi.Controllers
+{
+    public class FollowedTopicController(IFollowTopicService service) : BaseController
+    {
+        [HttpPost(nameof(AddAsync))]
+        public async Task<FollowedTopicDto> AddAsync(FollowedTopicDto dto)
+        {
+            return await service.AddAsync(dto);
+        }
+
+        [HttpGet(nameof(GetAll))]
+        public async Task<IReadOnlyCollection<FollowedTopicDto>> GetAll(Guid userId)
+        {
+            return await service.GetAllFollowedTopicsAsync(userId);
+        }
+
+        [HttpDelete(nameof(DeleteAsync))]
+        public async Task<bool> DeleteAsync(Guid? userId, DeleteType type, long followTopicId)
+        {
+            // TODO: second delete variant
+            return await service.DeleteAsync(followTopicId, type);
+        }
+}
+}
