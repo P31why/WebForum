b60c3f47d44d190ec996e832add1efb44cba4644
Hidan Entity <killer.xtale.suns@mail.ru>
2026-06-22 23:26:19 +0300

    добавлен маппер для пользователя

---
+++ WebForum.Data/Mappers/UserMapper.cs
@@ -0,0 +1,46 @@
+﻿
+using Riok.Mapperly.Abstractions;
+using WebForum.Core.Models;
+using WebForum.Data.Entities;
+
+namespace WebForum.Infrastructure.Mappers
+{
+    [Mapper]
+    public partial class UserMapper
+    {
+        [UserMapping]
+        public User DtoToEntity(UserDto dto)
+        {
+            return new User
+            {
+                Id = dto.Id,
+                Email = dto.Email,
+                UserName = dto.UserName,
+                IsDeleted = dto.IsDeleted,
+            };
+        }
+
+        [UserMapping]
+        public UserDto EntityToDto(User entity)
+        {
+            return new UserDto
+            {
+                Id = entity.Id,
+                UserName = entity.UserName,
+                Email = entity.Email,
+                IsDeleted = entity.IsDeleted,
+            };
+        }
+
+        [UserMapping]
+        public UserShortDto EntityToShortDto(User entity)
+        {
+            return new UserShortDto
+            {
+                Id = entity.Id,
+                IsDeleted = entity.IsDeleted,
+                UserName = entity.UserName
+            };
+        }
+    }
+}
