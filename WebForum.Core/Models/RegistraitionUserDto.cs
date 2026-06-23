cedf7907691f55b2cdb6214164f50055d2a20701
P31Dead <P31Dead@DESKTOP-44E946F>
2026-06-23 16:37:59 +0300

    email теперь не обязателен

--- WebForum.Core/Models/RegistraitionUserDto.cs
+++ WebForum.Core/Models/RegistraitionUserDto.cs
@@ -5,7 +5,7 @@ namespace WebForum.Core.Models
     {
         public required string UserName { get; set; }
 
-        public required string Email { get; set; }
+        public string? Email { get; set; }
 
         public required string Password { get; set; }
     }
