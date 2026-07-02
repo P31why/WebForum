3807112d331ba5b0ddcaa1cabef2876ff780f2cb
Hidan Entity <killer.xtale.suns@mail.ru>
2026-07-02 18:49:26 +0300

    добавлена аунтификация, регистрация пользователя, сброс пароля и измененвходной параметр в методе создания пользователя

--- WebForum.Application/User/Interfaces/IUserService.cs
+++ WebForum.Application/User/Interfaces/IUserService.cs
@@ -1,18 +1,21 @@
 ﻿
 using WebForum.Core;
 using WebForum.Core.Models;
+using WebForum.Core.RequestModels;
 
 namespace WebForum.Application.User.Interfaces
 {
     public interface IUserService
     {
-        public Task<UserDto> GetByIdAsync(Guid userId);
+        public Task<UserDto?> GetByIdAsync(Guid userId);
 
-        public Task<UserDto> AddAsync(UserDto dto);
+        public Task<UserDto> AddAsync(RegistraitionUserDto dto);
+
+        public Task<string> LoginUserAsync(AuthModel model);
 
         public Task<bool> UpdateAsync(UserDto dto);
 
-        public Task<bool> UpdatePasswordAsync(Guid id, string hash);
+        public Task<bool> UpdatePasswordAsync(Guid id, string newPawssword);
 
         public Task<bool> DeleteAsync(Guid userId, DeleteType type);
     }
--- WebForum.Application/User/Services/UserService.cs
+++ WebForum.Application/User/Services/UserService.cs
@@ -1,22 +1,55 @@
 ﻿
+using Microsoft.AspNetCore.Identity;
 using WebForum.Application.User.Interfaces;
 using WebForum.Core;
 using WebForum.Core.Models;
+using WebForum.Core.RequestModels;
 using WebForum.Infrastructure.Interfaces;
 using WebForum.Infrastructure.Mappers;
 
 namespace WebForum.Application.User.Services
 {
-    public class UserService(IUserRepository repository, UserMapper mapper) : IUserService
+    public class UserService(IUserRepository repository,
+                             IPasswordHasher<string> passwordHasher,
+                             IJwtProvider jwtProvider,
+                             UserMapper mapper) : IUserService
     {
-        public async Task<UserDto> AddAsync(UserDto dto)
+        public async Task<UserDto> AddAsync(RegistraitionUserDto dto)
         {
-            var entity = await repository.CreateEntityAsync(mapper.DtoToEntity(dto));
+            bool userExist = await repository.UserExistByNameAsync(dto.UserName);
 
-            if (entity == null)
-                throw new Exception("User creating error");
+            if (userExist)
+                throw new Exception("That username exsist");
+
+            dto.Password = passwordHasher.HashPassword(dto.UserName, dto.Password);
+
+            var entity = mapper.RegisterDtoToEntity(dto);
+
+            await repository.CreateEntityAsync(entity);
+
+            bool isCreated = await repository.CommitDbAsync();
+
+            if (!isCreated)
+                throw new Exception("Error creating user");
 
             return mapper.EntityToDto(entity);
+        }
+
+        public async Task<string> LoginUserAsync(AuthModel model)
+        {
+            var user = await repository.GetLoginDtoByNameAsync(model.UserName);
+
+            if(user == null)
+                throw new Exception("User is not exist");
+
+            var result = passwordHasher.VerifyHashedPassword(model.UserName, user.PasswordHash, model.Password);
+
+            if (result == PasswordVerificationResult.Failed)
+                throw new Exception("Login failed");
+
+            var userDto = await repository.GetDtoAsync(user.UserId);
+
+            return jwtProvider.GenerateUser(userDto!);
         }
 
         public async Task<bool> DeleteAsync(Guid userId, DeleteType type)
@@ -24,7 +57,7 @@ namespace WebForum.Application.User.Services
             return await repository.DeleteEntityAsync(userId, type);
         }
 
-        public async Task<UserDto> GetByIdAsync(Guid userId)
+        public async Task<UserDto?> GetByIdAsync(Guid userId)
         {
             return await repository.GetDtoAsync(userId);
         }
@@ -33,10 +66,14 @@ namespace WebForum.Application.User.Services
         {
             return await repository.UpdateUserEntityAsync(dto, UserModelType.Full);
         }
-
-        public async Task<bool> UpdatePasswordAsync(Guid id, string hash)
+         
+        public async Task<bool> UpdatePasswordAsync(Guid id, string newPawssword)
         {
-            return await repository.UpdateUserPasswordAsync(id, hash);
+            var user = await repository.GetShortDtoAsync(id);
+            
+            newPawssword = passwordHasher.HashPassword(user!.UserName, newPawssword);
+            
+            return await repository.UpdateUserPasswordAsync(id, newPawssword);
         }
     }
 }
