2a4e5de217149645993aae2d5fa21fc9f826dd64
Hidan Entity <killer.xtale.suns@mail.ru>
2026-07-02 18:42:19 +0300

    новая модель для логина пользователя

---
+++ WebForum.Core/Models/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace WebForum.Core.Models
+{
+    public class LoginDto
+    {
+        public required Guid UserId { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
