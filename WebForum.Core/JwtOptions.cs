70b5d237ff17985971576a5a9d721f8249ef9bef
Hidan Entity <killer.xtale.suns@mail.ru>
2026-07-02 18:46:13 +0300

    класс для хранения взаимодействя с параметрами конфига

---
+++ WebForum.Core/JwtOptions.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebForum.Core
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; set; } = string.Empty;
+
+        public int ExpiresTime { get; set; }
+    }
+}
