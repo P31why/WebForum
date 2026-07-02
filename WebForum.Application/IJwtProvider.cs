2a5af755ce5abd53713515f302c9963e982d5433
Hidan Entity <killer.xtale.suns@mail.ru>
2026-07-02 18:44:00 +0300

    добавлен класс для работы с jwt авторизацией

---
+++ WebForum.Application/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿
+using WebForum.Core.Models;
+
+namespace WebForum.Application
+{
+    public interface IJwtProvider
+    {
+        public string GenerateUser(UserDto dto);
+    }
+}
---
+++ WebForum.Application/JwtProvider.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebForum.Core;
+using WebForum.Core.Models;
+
+namespace WebForum.Application
+{
+    public class JwtProvider(IOptions<JwtOptions> options): IJwtProvider
+    {
+        private readonly JwtOptions _options = options.Value;
+
+        public string GenerateUser(UserDto dto)
+        {
+            Claim[] claims = [new("userId", dto.Id.ToString())];
+
+            var creds = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)), 
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                signingCredentials: creds,
+                expires: DateTime.UtcNow.AddMinutes(_options.ExpiresTime));
+            
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
