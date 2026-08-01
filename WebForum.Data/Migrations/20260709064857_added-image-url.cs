47aa54dbd18af31686a8a54e1638a82ace8294d9
P31Dead <P31Dead@DESKTOP-44E946F>
2026-08-01 20:43:56 +0300

    добавлена миграция на ссылки с картинками

---
+++ WebForum.Data/Migrations/20260709064857_added-image-url.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebForum.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedimageurl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                schema: "public",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                schema: "public",
+                table: "FollowedTopics",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                schema: "public",
+                table: "Comments",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                schema: "public",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                schema: "public",
+                table: "FollowedTopics");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                schema: "public",
+                table: "Comments");
+        }
+    }
+}
